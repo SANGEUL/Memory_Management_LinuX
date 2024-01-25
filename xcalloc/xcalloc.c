4c68fdb9e486b86300f322c1a4906c1a08c7b5b5
Sangeeta_M <139239874+SANGEUL@users.noreply.github.com>
2024-01-25 21:10:14 +0530

    use of  malloc() wrapper that prints an error message and terminates the program if         malloc( ) returns NULL. By convention, developers call this common wrapper xcalloc( )

--- xcalloc/xcalloc.c
+++ xcalloc/xcalloc.c
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/fs.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <errno.h>
+void * xcalloc0 (size_t);
+
+int main()
+{
+    void *q;
+    q = xcalloc0(10);
+
+    if(q)
+        printf("\n Successfull xcalloc() \n");
+}
+
+/* like malloc( ), but zeros memory and terminates on failure */
+void * xcalloc0 (size_t size)
+{
+    void *p;
+    p = calloc(1, size);
+    if(!p)
+    {
+        perror ("xmalloc0");
+        exit (EXIT_FAILURE);
+    }
+    return p;
+}
