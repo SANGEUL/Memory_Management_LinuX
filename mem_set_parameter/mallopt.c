e67fa257726c22fba9491b0b24bf97b75c1dcf57
Sangeeta_M <139239874+SANGEUL@users.noreply.github.com>
2024-01-27 19:32:02 +0530

    A call to mallopt() sets the memory-management-related parameter specified by param to the value specified by value .

---
+++ mem_set_parameter/mallopt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/fs.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <errno.h>
+#include <malloc.h>
+
+int mallopt (int param, int value);
+
+int main()
+{
+    int ret;
+    /* use mmap( ) for all allocations over 64 KB */
+    ret = mallopt (M_MMAP_THRESHOLD, 64 * 1024);
+    if (!ret)
+    fprintf (stderr, "mallopt failed!\n");
+    
+    else
+        printf("\n use mmap( ) for all allocations over 64 KB \n");
+}
